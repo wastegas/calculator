7af15767506254216a6ecf9bcc6601ef04ca40cf
wastegas <wastegas@gmail.com>
2017-05-12 20:18:19 -1000

    add test for SLICE_EXPRESSION

--- __test__/reducers/updateExpression.test.js
+++ __test__/reducers/updateExpression.test.js
@@ -15,4 +15,27 @@ describe('expressionReducer', () => {
 			}
 		)
 	})
+	expect(
+			expressionReducer({expression:"1+"},
+					{
+						type: types.UPDATE_EXPRESSION,
+						payload: "1"
+					})
+			).toEqual(
+				{
+					expression: "1+1"
+				}
+			)
+	it('should handle SLICE_EXPRESSION', () => {
+		expect(
+			expressionReducer({expression:"1+1"},
+					{
+						type: types.SLICE_EXPRESSION
+					})
+			).toEqual(
+				{
+					expression: "1+"
+				}
+			)
+	})
 })
