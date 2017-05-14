771eacd5a52262ceb201a00df7ce6c9fad0dbec1
wastegas <wastegas@gmail.com>
2017-05-14 11:16:59 -1000

    add initial component shell

---
+++ src/components/calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+	render() {
+		return (
+			<p>A calculator will go here</p>
+		)
+	}
+}
+
+export default Calculator;
