c9ef73a01bc14d13262eef3e5149b0d1064ddddf
wastegas <wastegas@gmail.com>
2017-05-15 22:20:53 -1000

    add middleware thunk and logger

--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
 import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducers from './reducers';
 
-export default createStore(reducers);
+const middleware = applyMiddleware(thunk, logger);
+
+export default createStore(reducers, middleware);
