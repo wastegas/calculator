78249a022b5bf9975b73829c819c8f56a28772ac
wastegas <wastegas@gmail.com>
2017-05-15 21:43:12 -1000

    use action creator pattern

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+import * as types from '../constants/actionTypes';
+
+export function updateExpression(text) {
+	return { type: types.UPDATE_EXPRESSION, payload: text}
+}
+
+export function sliceExpression() {
+	return { type: types.SLICE_EXPRESSION }
+}
+
+export function clearExpression() {
+	return { type: types.CLEAR_EXPRESSION }
+}
+
+export function updateDisplay(text) {
+	return {
+		type: types.UPDATE_DISPLAY,
+		payload: text
+	}
+}
+
+export function resultDisplay(text) {
+	return {
+		type: types.RESULT_DISPLAY,
+		payload: eval(text)
+	}
+}
+
+export function clearDisplay() {
+	return {
+		type: types.CLEAR_DISPLAY
+	}
+}
