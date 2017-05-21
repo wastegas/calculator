98c3ce07c15d40b4fad661604359fb590cc68893
wastegas <wastegas@gmail.com>
2017-05-20 20:52:54 -1000

    suppress operators on the display

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,11 +17,18 @@ export function updateDisplay(text) {
 	return(dispatch,getState) => {
 		const { firstentry } = getState().firstentry;
 		if (firstentry) { 
-			dispatch({type: types.TOGGLE_FIRSTENTRY})
-			dispatch({type: types.FIRSTENTRY_DISPLAY , payload: text})
-		} else
-			dispatch({type: types.UPDATE_DISPLAY, payload: text})
-		dispatch({type: types.UPDATE_EXPRESSION, payload: text})
+			dispatch({type: types.TOGGLE_FIRSTENTRY});
+			dispatch({type: types.FIRSTENTRY_DISPLAY , payload: text});
+		} else {
+			const { expression } = getState().expression;
+			if (parseInt(text) || text === '.') {
+				dispatch({type: types.UPDATE_DISPLAY, payload: text});
+			}
+		}
+		dispatch({type: types.UPDATE_EXPRESSION, payload: text});
+		if (text != '.' && !parseInt(text)) {
+			dispatch({type: types.TOGGLE_FIRSTENTRY});
+		}
 	}
 }
 
