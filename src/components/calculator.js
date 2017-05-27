6458d8769832c7e665be96b1b15ebbc9013e0ae2
wastegas <wastegas@gmail.com>
2017-05-26 22:59:06 -1000

    import Buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,96 +1,20 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import Display from './display';
-import * as actionCreators from '../actions/';
+import Buttons from '../containers/buttons';
 import '../styles.scss';
-
-function Keys(props) {
-	const keys = props.keys;
-	const item = keys.map((v, key) => 
-		 <button className={v.oper} key={key} onClick={() => props.onClick(v.val)}>{v.val}</button>
-	);
-		return(<div className='keys'>{item}</div>)
-}
-
-class Buttons extends Component {
-			
-	//handleClick(v) {
-	//	this.props.action.updateExpression(v);
-	//	this.props.action.updateDisplay(v);
-	//}
-	render() {
-			const keys = [
-				{val:"AC", oper:"func"},
-				{val:"C",  oper:"func"},
-				{val:"(",  oper:"func"},
-				{val:")",  oper:"func"},
-				{val:"7",  oper:"num"},
-				{val:"8",  oper:"num"},
-				{val:"9",  oper:"num"},
-				{val:"/",  oper:"oper"},
-				{val:"4",  oper:"num"},
-				{val:"5",  oper:"num"},
-				{val:"6",  oper:"num"},
-				{val:"*",  oper:"oper"},
-				{val:"1",  oper:"num"},
-				{val:"2",  oper:"num"},
-				{val:"3",  oper:"num"},
-				{val:"-",  oper:"oper"},
-				{val:"0",  oper:"num"},
-				{val:".",  oper:"num"},
-				{val:"=",  oper:"oper"},
-				{val:"+",  oper:"oper"}
-			]
-return (
-			<Keys keys={keys} onClick={(n) => this.props.onClick(n)} />
-		)
-	}
-}
 
 class Calculator extends Component {
 	constructor(props) {
 		super(props);
 	}
-	handleClick(v) {
-		if (v === 'AC') {
-			this.props.action.resetCalculator();
-			return;
-		}
-		if (v === 'C') {
-			this.props.action.sliceExpression();
-			return;
-		}
-		if (v === '=') {
-			this.props.action.resultDisplay();
-			return;
-		}
-		this.props.action.updateDisplay(v);
-	}
 	render() {
-
-		const { expression } = this.props;
-
 		return (
 			<div className='calculator'>
 				<Display  />
-				<Buttons onClick={(n) => this.handleClick(n)}/>
+				<Buttons />
 			</div>
 		)
 	}
 }
 
-function mapStateToProps(state, prop) {
-	return {
-		expression: state.expression,
-	}
-}
-
-
-function mapDispatchToProps(dispatch) {
-	return {
-		action: bindActionCreators(actionCreators, dispatch)
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
+export default Calculator;
